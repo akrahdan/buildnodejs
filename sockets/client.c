8f1d0b136c206cc5ed82e9bb7a0b084ebf6b9c7c
Sakrah <sakrah@Sakrahs-Mac.local>
2016-06-29 12:37:28 +0000

    Adding include files

---
+++ sockets/client.c
@@ -0,0 +1,62 @@
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#define PORT "3490" // the server port client will be connecting to
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+// get sockaddr, IPv4 or IPv6:
+
+int main(int argc, char *argv[])
+{
+	int sockfd, numbytes;
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET_ADDRSTRLEN];
+	if (argc != 2) {
+		fprintf(stderr, "usage: client hostname\n");
+		exit(1);
+	}
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	// loop through all the results and connect to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+		break;
+	}
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+    
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr
+
+	inet_ntop(p->ai_family, &(ipv4->sin_addr),
+		s, sizeof s);
+
+	printf("client: connecting to %s\n", s);
+	freeaddrinfo(servinfo); // all done with this structure
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+	buf[numbytes] = '\0';
+	printf("client: received '%s'\n", buf);
+	close(sockfd);
+	return 0;
+}
---
+++ sockets/server.c
@@ -0,0 +1,89 @@
+/*
+** server.c -- a stream socket server demo
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+
+#define PORT "3000" // the port users will be connecting to
+#define BACKLOG 10 // the maximum number of pending connections queue will hold
+
+int main(void)
+{
+	int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_in client; // client's address information
+	socklen_t sin_size;
+	int yes = 1;
+	char s[INET_ADDRSTRLEN];
+	int rv;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+	// loop through all the results and bind to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+			sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(servinfo); // all done with this structure
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+	while (1) { // main accept() loop
+		sin_size = sizeof client;
+	  new_fd = accept(sockfd, (struct sockaddr *)&client, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+		inet_ntop(client.sin_family,
+			&(client.sin_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+		if (!fork()) { // this is the child process
+          char buf[] = "This is your first NodeJs Server \n";
+			close(sockfd); // child doesn't need the listener
+			if (send(new_fd, buf, sizeof buf, 0) == -1)
+				perror("send");
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd); // parent doesn't need this
+	}
+	return 0;
+}
