47d6f4b5f196e1a7f6ae442777ec31c85274403a
Sakrah <sakrah@Sakrahs-Mac.local>
2016-07-05 08:35:01 +0000

    Changes to thread.c

--- uv/src/unix/thread.c
+++ uv/src/unix/thread.c
@@ -1,6 +1,3 @@
-
-
-
 #include "uv.h"
 #include "internal.h"
 
@@ -38,53 +35,11 @@ static void* uv__thread_start(void *arg)
 
 
 int uv_thread_create(uv_thread_t *tid, void (*entry)(void *arg), void *arg) {
-  struct thread_ctx* ctx;
+  
   int err;
-  pthread_attr_t* attr;
-#if defined(__APPLE__)
-  pthread_attr_t attr_storage;
-  struct rlimit lim;
-#endif
-
-  ctx = uv__malloc(sizeof(*ctx));
-  if (ctx == NULL)
-    return UV_ENOMEM;
-
-  ctx->entry = entry;
-  ctx->arg = arg;
-
-  /* On OSX threads other than the main thread are created with a reduced stack
-   * size by default, adjust it to RLIMIT_STACK.
-   */
-#if defined(__APPLE__)
-  if (getrlimit(RLIMIT_STACK, &lim))
-    abort();
-
-  attr = &attr_storage;
-  if (pthread_attr_init(attr))
-    abort();
-
-  if (lim.rlim_cur != RLIM_INFINITY) {
-    /* pthread_attr_setstacksize() expects page-aligned values. */
-    lim.rlim_cur -= lim.rlim_cur % (rlim_t) getpagesize();
-
-    if (lim.rlim_cur >= PTHREAD_STACK_MIN)
-      if (pthread_attr_setstacksize(attr, lim.rlim_cur))
-        abort();
-  }
-#else
-  attr = NULL;
-#endif
-
-  err = pthread_create(tid, attr, uv__thread_start, ctx);
-
-  if (attr != NULL)
-    pthread_attr_destroy(attr);
-
-  if (err)
-    uv__free(ctx);
-
-  return -err;
+  
+  err = pthread_create(tid, NULL, entry, arg);
+  return err;
 }
 
 
@@ -159,26 +114,7 @@ void uv_mutex_unlock(uv_mutex_t* mutex) {
 
 
 
-int uv_rwlock_tryrdlock(uv_rwlock_t* rwlock) {
-  int err;
 
-  err = pthread_rwlock_tryrdlock(rwlock);
-  if (err) {
-    if (err != EBUSY && err != EAGAIN)
-      abort();
-    return -EBUSY;
-  }
-
-  return 0;
-}
-
-
-
-
-void uv_rwlock_wrunlock(uv_rwlock_t* rwlock) {
-  if (pthread_rwlock_unlock(rwlock))
-    abort();
-}
 
 
 void uv_once(uv_once_t* guard, void (*callback)(void)) {
